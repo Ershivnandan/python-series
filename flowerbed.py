feb1162fc7d429ec92909e86b5d4feaa099262ff
ErShivnandan <ec1912@global.org.in>
2024-09-06 22:44:32 +0530

    06-09-2024

--- flowerbed.py
+++ flowerbed.py
@@ -1,21 +1,29 @@
-def flowerbed(arr, n):
+def flowerbed(arr, n,m):
     # res= [0] * n
     for i in range(n):
-
-        if i == 0 and arr[i+1] == 0 or i == n-1 and arr[i-1] == 0:
+        if m <= 0:
+            break
+        elif i == 0 and arr[i+1] == 0 or i == n-1 and arr[i-1] == 0:
             arr[i] = 1
+            m -= 1
         elif arr[i] == 0 and arr[i-1] == 0 and arr[i+1] == 0:
             arr[i] = 1
+            m -= 1
         else:
             continue
     
     # flag = True
     # for i in range(n):
+    # print(*arr)
+    if m <= 0:
+        print(True)
+    else:
+        print(False)
 
 
 
-arr = [0,0,1,0,1,0,0,0,1]
+arr = [0,0,1,1,1,0,0,0,1]
 
 n = len(arr)
 
-flowerbed(arr, n)
+flowerbed(arr, n, 1)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-20919
+const numbers = [1, 2, 3, 4, 5]
+
+const [a, b, c, ...rest] = numbers;
+
+console.log(rest);
